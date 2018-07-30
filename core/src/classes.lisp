d59eb4a4df8d8fb15a2ad35bdbfcd98e5a2c9358
yanqirenshi <yanqirenshi@gmail.com>
2018-07-30 10:08:47 +0900

    add jojo:%to-json

--- core/src/classes.lisp
+++ core/src/classes.lisp
@@ -9,12 +9,25 @@
 (defclass mutable (immutable)
   ((updated-at :accessor updated-at :initarg :updated-at :initform nil)))
 
+;;; node
 (defclass node (shinra:shin) ())
 
+;;; edge
 (defclass edge (shinra:ra immutable)
   ((start :accessor start :initarg :start :initform nil)))
+
 (defclass edge-past (edge)
   ((end :accessor end :initarg :end :initform nil)))
+
+(defmethod jojo:%to-json ((obj edge))
+  (jojo:with-object
+    (jojo:write-key-value "start" (slot-value obj 'start))))
+
+(defmethod jojo:%to-json ((obj edge-past))
+  (jojo:with-object
+    (jojo:write-key-value "start" (slot-value obj 'start))
+    (jojo:write-key-value "end"   (slot-value obj 'end))))
+
 
 
 ;;;;;
@@ -23,6 +36,11 @@
 (defclass term (node immutable) (start end))
 (defclass schedule (term) ())
 (defclass result (term) ())
+
+(defmethod jojo:%to-json ((obj term))
+  (jojo:with-object
+    (jojo:write-key-value "start" (slot-value obj 'start))
+    (jojo:write-key-value "end"   (slot-value obj 'end))))
 
 
 ;;;;;
@@ -40,3 +58,24 @@
 (defclass workpackage (tree-node) ())
 
 (defclass artifact (tree-node) ())
+
+(defmethod jojo:%to-json ((obj project))
+  (jojo:with-object
+    (jojo:write-key-value "code"        (slot-value obj 'code))
+    (jojo:write-key-value "name"        (slot-value obj 'name))
+    (jojo:write-key-value "description" (slot-value obj 'description))))
+
+(defmethod jojo:%to-json ((obj wbs))
+  (jojo:with-object
+    (jojo:write-key-value "name"        (slot-value obj 'name))
+    (jojo:write-key-value "description" (slot-value obj 'description))))
+
+(defmethod jojo:%to-json ((obj workpackage))
+  (jojo:with-object
+    (jojo:write-key-value "name"        (slot-value obj 'name))
+    (jojo:write-key-value "description" (slot-value obj 'description))))
+
+(defmethod jojo:%to-json ((obj artifact))
+  (jojo:with-object
+    (jojo:write-key-value "name"        (slot-value obj 'name))
+    (jojo:write-key-value "description" (slot-value obj 'description))))
