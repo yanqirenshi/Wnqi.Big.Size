d7f7384f383f12b8766a2c6a91e6744172cff882
yanqirenshi <yanqirenshi@gmail.com>
2018-08-11 07:16:45 +0900

    fix description at %to-json

--- core/src/classes.lisp
+++ core/src/classes.lisp
@@ -80,26 +80,26 @@
     (jojo:write-key-value "_id"   (slot-value obj 'up:%id))
     (jojo:write-key-value "code"        (slot-value obj 'code))
     (jojo:write-key-value "name"        (slot-value obj 'name))
-    (jojo:write-key-value "description" (slot-value obj 'description))
+    (jojo:write-key-value "description" (or (slot-value obj 'description) ""))
     (jojo:write-key-value "_class"      "PROJECT")))
 
 (defmethod jojo:%to-json ((obj wbs))
   (jojo:with-object
     (jojo:write-key-value "_id"   (slot-value obj 'up:%id))
     (jojo:write-key-value "name"        (slot-value obj 'name))
-    (jojo:write-key-value "description" (slot-value obj 'description))
+    (jojo:write-key-value "description" (or (slot-value obj 'description) ""))
     (jojo:write-key-value "_class"      "WBS")))
 
 (defmethod jojo:%to-json ((obj workpackage))
   (jojo:with-object
     (jojo:write-key-value "_id"   (slot-value obj 'up:%id))
     (jojo:write-key-value "name"        (slot-value obj 'name))
-    (jojo:write-key-value "description" (slot-value obj 'description))
+    (jojo:write-key-value "description" (or (slot-value obj 'description) ""))
     (jojo:write-key-value "_class"      "WORKPACKAGE")))
 
 (defmethod jojo:%to-json ((obj artifact))
   (jojo:with-object
     (jojo:write-key-value "_id"   (slot-value obj 'up:%id))
     (jojo:write-key-value "name"        (slot-value obj 'name))
-    (jojo:write-key-value "description" (slot-value obj 'description))
+    (jojo:write-key-value "description" (or (slot-value obj 'description) ""))
     (jojo:write-key-value "_class"      "ARTIFACT")))
