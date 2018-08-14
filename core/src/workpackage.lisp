51fca26589fb4ac656a3b1e0343ac475627ca028
yanqirenshi <yanqirenshi@gmail.com>
2018-08-14 10:18:56 +0900

    update tx-make-workpackage

--- core/src/workpackage.lisp
+++ core/src/workpackage.lisp
@@ -4,7 +4,12 @@
   (find-if #'(lambda (d) (= %id (%id d)))
            (find-vertex graph 'workpackage)))
 
-(defun tx-make-workpackage (graph name &key description)
+(defun %tx-make-workpackage (graph name description)
   (tx-make-vertex graph 'workpackage
                   `((name ,name)
                     (description ,description))))
+
+(defun tx-make-workpackage (graph name &key description)
+  (let ((wp (%tx-make-workpackage graph name description)))
+    (tx-add-schedule graph wp nil nil)
+    (tx-add-result graph wp nil nil)))
